d22e71cb26d21db1c5ff51b912a4ac924c1cb204
Jun1380 <91740365+Jun1380@users.noreply.github.com>
2021-10-04 00:02:17 +0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,148 @@
+// Bai 3a
+// for ( let x = 0; x < 7; x++){
+//     console.log(x);
+// }
+
+// Bai 3b
+// let n = prompt(' Enter a number');
+// for (let x = 0; x < n ; x++){
+//     console.log(x);
+// }
+
+// Bai 3c
+//  let n = prompt(' Enter a number');
+// for (let x = 3; x < n ; x++){
+//     console.log(x);
+// }
+
+
+//bai3d
+    
+// let n = prompt('Enter n');
+// let c = prompt('Enter c');
+// for (; c < n; c++){
+//     console.log(c);
+// }
+
+//bai3e
+ 
+//    let n = Number(prompt(' Enter n'));
+//    let c = Number(prompt(' Enter c'));
+//    for(; c < n; c += 3){
+//        console.log(c);
+//    }
+
+//bai3f
+
+//   let n = Number(prompt('Enter n'));
+//   let c = Number(prompt(' Enter c '));
+//   let s = Number(prompt('Enter  s'));
+//   for(; c<n ; c+=s ){
+//       console.log(c);
+//       }
+
+//bai4
+
+// let n= Number(prompt('Enter a number'));
+// let factorial = 1;
+// let c =1;
+// for(; c <= n; c++){
+//     factorial = factorial*c;
+// }
+// console.log("The factorial of "+ n + " is " + factorial);
+// alert("The factorial of "+ n + " is " + factorial);
+
+// bai 5
+// let age = prompt(' How old are you');
+// if (age < 14){
+//     console.log("You are not old enough to view this content");
+// } 
+// else {
+//     console.log ( "Enjoy ");
+// }
+    
+// bai 6
+// let x = prompt(' Enter a number ');
+// if ( x > 4,5){
+//     alert(`Higher half of 9`);
+// }
+// else{
+//     alert('Lower half of 9');
+
+
+// bai 7
+// let n = prompt(' n=');
+// let x = prompt(' x= ');
+// if(x<n/2){
+//     alert(' ${x} is lower half of ${n}');
+// }
+// else{
+//     alert(` ${x} is higher half of ${n}`);
+// }
+
+// bai 8
+// let x = prompt(" x =");
+// if (x%2 == 0){
+//     alert(` ${x} is an even number `);
+// }
+// else {
+//     alert(` ${x} is an odd number`);
+// }
+
+// bai 9a
+// let x = 6;
+// for ( let y = 0; y < x/2; y++){
+//     console.log("L");
+// }
+// for ( let z = 0; z< x/2; z++){
+//     console.log("H");
+// } 
+
+// bai 9b
+// let x = Number(prompt(" Enter the total number of L's and H's? "));
+// let y=0;
+// for ( ; y < x/2; y++)
+//     console.log('L');
+
+// for (let z = 0; z < x - y; z++)
+//     console.log('H');
+
+// bai 9c
+// let number = 8;
+// for(x=0 ; x < number; x++)
+// if (x % 2==0)
+//     console.log(0);
+
+// else 
+//     console.log(1);
+
+// bai 9d
+// let x = Number(prompt("Enter the total number of 1's and 0's? "));
+// for( y = 0; y< x; y++)
+// if(y%2==0){
+//     console.log(0);
+// } else {
+//     console.log(1);
+// }
+
+// Bai 10
+// let x = prompt ('Your weight in kg?')
+// let y = prompt ('Your height in cm?')
+// let z = x/((y/100)*(y/100))
+// alert ('Your BMI is ' + z)
+
+// if (z < 16)
+// {alert (' You are severely underweight');}
+
+// else if (z < 18.5)
+// {alert ('You are underweight');}
+
+// else if (z < 25)
+// {alert ('You are normal');}
+
+// else if (z < 30)
+// {alert ('You are overweight');}
+
+// else
+// {alert ('You are obese');}
+
