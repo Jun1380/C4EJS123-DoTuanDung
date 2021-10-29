5ce63f8659dc07bcaf910dd97f0555725d434d1d
Do Tuan Dung <91740365+Jun1380@users.noreply.github.com>
2021-10-29 22:08:02 +0700

    Add files via upload

--- test.js
+++ test.js
@@ -1,68 +1,31 @@
-// bai 1
-{
-let str_input = 'program';
-let reverse_str = '';
 
-for (let i= str_input.length -1; i >= 0; i--){
-  reverse_str += str_input[i];
-}
+    async function fetchThis() {
+  
+        const fetchData = await fetch('http://tobitheme.net/api/characters.json'); 
+        return await fetchData.json();
+    }
+    fetchThis().then(
+      data => {
+            document.getElementById('characters_name').innerHTML = data['data'][0]['name'];
+            document.getElementById('characters_species').innerHTML = data['data'][0]['species'];
+            document.getElementById('characters_gender').innerHTML = data['data'][0]['gender'];
+            document.getElementById('characters_house').innerHTML = data['data'][0]['house'];
+            document.getElementById('characters_dateOfBirth').innerHTML = data['data'][0]['dateOfBirth'];
+            document.getElementById('characters_yearOfBirth').innerHTML = data['data'][0]['yearOfBirth'];
+            document.getElementById('characters_ancestry').innerHTML = data['data'][0]['ancestry'];
+            document.getElementById('characters_eyeColour').innerHTML = data['data'][0]['eyeColour'];
+            document.getElementById('characters_hairColour').innerHTML = data['data'][0]['hairColour'];
+            document.getElementById('characters_wand').innerHTML = data['data'][0]['wand'];
+            document.getElementById('characters_patronus').innerHTML = data['data'][0]['patronus'];
+            document.getElementById('characters_hogwartsStudent').innerHTML = data['data'][0]['hogwartsStudent'];
+            document.getElementById('characters_actor').innerHTML = data['data'][0]['actor'];
+            document.getElementById('characters_hogwartsStaff').innerHTML = data['data'][0]['hogwartsStaff'];
+            document.getElementById('characters_hogwartsStaff').innerHTML = data['data'][0]['hogwartsStaff'];
+    
 
-console.log(reverse_str);
- 
-}
-
-
-// bai 2
-
-const noti = ' this is Test'
-
-const capitalize = str => {
-    if (str) return ''
-    return str.charAt(0).toUpperCase() +
-    str.slice(1)
-}
-
-
-
-{bai3
-let dayso = [1,2,3,3,4,5,4,4,4,4,5,5];
-let  update= new Set(dayso);
-console.log(update);
-}
-
-
-// Bai 4
-let person = [{
-    name: "Dung",
-    age: 18,
-salary: '30000$',
-position: 'Boss',},
-
-{name: 'Duong',
-age: 19,
-salary: '25000$',
-position:'Manager',},
-
-{name: 'Ton',
-age: 19,
-salary: '10000$',
-position: 'Staff',},
-
- ] ;
-
-console.log(person);
-
-let newPerson = prompt('Nhap ten nguoi moi');
-person.push({
-    newPerson,
-    age: 19,
-  salary: '10000$',
-  position: 'Teacher',
-});
-
-
-let  ask = prompt('age');
-let ask2 = prompt('salary');
-let ask3 = prompt('position');
-person['ask', 'ask 2', 'ask 3'] = updateProfile;
-
+      
+      
+      
+        }
+        );
+    
