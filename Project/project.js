23874bdcb3956bdbf69b497c11e5ff49362bf191
Do Tuan Dung <91740365+Jun1380@users.noreply.github.com>
2021-10-31 10:49:30 +0700

    Add files via upload

---
+++ Project/project.js
@@ -0,0 +1,113 @@
+let dogiadinh =[
+    {  
+        ten : 'Chảo inox chống dính K:-326',
+        xuatxu : 'Thái Lan',
+        chatlieu: 'Inox',
+        duongkinh: '26cm',
+        sudung: 'Bếp từ, bếp ga, bếp hồng ngoại, bếp từ',
+        price: '1.290.000vnd',
+
+    },
+
+    { 
+        ten :'Nồi inox 3 đáy nắp inox 24cm',
+        xuatxu: 'Việt Nam',
+        chatlieu: 'Inox 430 cao cấp',
+        duongkinh: '24cm',
+        sudung:'Đáy 3 lớp trên bếp gas, bếp hồng ngoại và bếp từ',
+        price: '245.000vnd',
+
+    },
+
+    {
+        ten: 'Bộ lau nhà BH-M950A',
+        xuatxu: 'Trung Quốc',
+        chatlieu: 'sợi Microfiber, thân cây lau bằng inox , thùng nước bằng nhựa',
+        cautao:'Đầu lau xoay 360 độ',
+        price: '300.000vnd',
+        
+ 
+    },
+
+    {
+        ten:'Vòi rửa chén tăng áp EL-RC07',
+        xuatxu: 'Trung Quốc',
+        chatlieu: 'Inox SUS',
+        cautao:'Cần quay 360 độ, vòng quay rộng, đầu thiết kế với công nghệ mới',
+        price: '400.000vnd',
+
+    },
+]
+
+let maylockhongkhi = [
+    {
+        ten: 'Máy lọc không khí HAKAWA HK-K2',
+        xuatxu: 'Nhật Bản',
+        hieunang: 'Công suất 67W',
+        dacdiem: 'Điều khiển từ xa trên smartphone, tiết kiệm điện, công nghệ Plasma ion',
+        price:'3.000.000vnd',
+    },
+
+    {
+        ten:'Máy lọc không khí CF-8116',
+        xuatxu:'Trung Quốc',
+        kichthuoc:'Ngang 21,5cm - Cao 37,5cm - Sâu 23,1cm-0,7kg',
+        congsuat:'30W',
+        cautao:'Đèn UV diệt khuẩn, màng lọc thô,hepa, than hoạt tính',
+        price:'5.800.000vnd',
+
+    }
+
+];
+
+let lovisong = [
+    {
+        ten: 'Lò vi sóng có nướng Hafele HW-F30A',
+        xuatxu:'Trung Quốc',
+        congsuatvisong:'900W',
+        congsuatnuong:'1100W',
+        chatlieu:'Thép không gỉ',
+        kichthuoc:'Ngang 52cm - Cao 32,6cm- Sâu 46,7cm',
+        price:'4.893.000vnd',
+
+    },
+
+    {
+        ten:'Lò vi sóng lắp âm Modena BV3435 40 lít',
+        xuatxu:'Indonesia',
+        congsuatvisong:'1000W',
+        congsuatnuong:'1500W',
+        chatlieu:'Thép không gỉ',
+        kichthuoc:'Ngang 59.5 cm - Cao 45.5 cm - Sâu 61 cm - Nặng 36 kg',
+        bangdieukhien:'Cảm ứng kèm màn hình hiển thị',
+        chucnang: 'Khoá trẻ em, Hẹn giờ, Chuông báo khi nấu xong',
+        price:'24.074.000vnd',
+
+    },
+]
+
+let dolamsinhto =[
+    {
+        ten:'Máy làm sữa hạt đa năng Unie V6',
+        xuatxu:'Việt Nam',
+        dungtich:'1.75l',
+        congsuat:'Xay 1000W- Nấu 800W',
+        chatlieu:'Thép không gỉ, thuỷ tinh chịu lực, inox 304',
+        chucnang:'Hấp, hầm, nấu súp, sinh tố, cháo,...',
+        tienich:'Hẹn giờ,chống trào,Cảm ứng, chống trượt',
+        kichthuoc:'Cao 45 cm - Ngang 33 cm - Sâu 27.5 cm - Nặng 5.4 kg',
+        price:'2.480.000vnd',
+    },
+
+    {
+        ten:'Máy xay sinh tố Electrolux EBR3416',
+        xuatxu:'Thuỵ Điển',
+        dungtich:'Cối lớn 1.25l - Cốc xay Không có - Cối nhỏ 0.2l - Cối xay thịt Không có',
+        congsuat:'400W',
+        chatlieu:'Nhựa, thép không gỉ',
+        tinhnang:'Tự ngắt khi quá tải, chống trượt',
+        tocdoxay:'3 tốc độ, 1 nút nhồi để trộn nguyên liệu',
+        price:'595.000vnd',
+    },
+]
+
